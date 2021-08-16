e1f6d0eab523f24cca75d4b417623d898f9ac74a
Chris0024 <chrisfa1998@gmail.com>
2021-08-16 16:47:59 -0500

    funcion añadida

--- humano.py
+++ humano.py
@@ -2,14 +2,11 @@ class humano():
     def __init__(self,nombre,edad):
         self.nombre = nombre
         self.edad = edad
-<<<<<<< HEAD
-    def hacerpopo(self):
-        print("me hize popo")
-=======
     def Aumento_Edad (self):
         Aumento = int(input("¿Cuanto de edad quieres aumentar? :"))
         self.edad += Aumento
         
     def hacerpopo(self):
         print("me hize popo")
->>>>>>> 86b554dd73ad5ad18ee56be0f1805952d5eb50ca
+    def numero(self):
+        print(2)
