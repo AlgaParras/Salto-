56864516373a32d7f2195cb7852b34c1eea8f2e9
AlgaParras <fabrizzio.miranda@inacapmail.cl>
2021-08-16 17:51:04 -0400

    Uso de humano.

---
+++ Usahumano.py
@@ -0,0 +1,10 @@
+import humano as Hu
+
+Federico = Hu.humano("Federico",33)
+
+
+cagar = int(input("¿Cuantas veces quieres cagar? :"))
+
+while cagar >= 0:
+    Federico.hacerpopo()
+    cagar -= 1 
