a8521da40a2cc1de8bcccdec12b145126360908f
AlgaParras <fabrizzio.miranda@inacapmail.cl>
2021-08-16 16:58:56 -0400

    humano.py

--- humano.py
+++ humano.py
@@ -2,3 +2,7 @@ class humano():
     def __init__(self,nombre,edad):
         self.nombre = nombre
         self.edad = edad
+    def Aumento_Edad (self):
+        Aumento = int(input("¿Cuanto de edad quieres aumentar? :"))
+        self.edad += Aumento
+        
